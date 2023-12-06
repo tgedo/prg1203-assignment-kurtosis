b600a77cab8593b65ab31f6c798852db3186ab57
Alex Chee <xxchee555xx@gmail.com>
2023-12-07 00:07:47 +0800

    ✏️ added setters and getters to User

--- src/User.java
+++ src/User.java
@@ -16,8 +16,78 @@ public class User {
         // TODO figure this part out thanks
     }
     
-    public void AddPokemonIntoDeck(Pokemon name){
-        deck.add(name);
+    public void AddPokemonIntoDeck(Pokemon pokemon){
+        deck.add(pokemon);
+    }
+
+    public static ArrayList getUserIDList() {
+        return userIDList;
+    }
+
+    public static void addUserIDList(String userID) {
+        User.userIDList.add(userID);
+    }
+
+    public static ArrayList getUserNameList() {
+        return userNameList;
+    }
+
+    public static void addUserNameList(String userName) {
+        User.userNameList.add(userName)
+    }
+
+    public static ArrayList getUserPassList() {
+        return userPassList;
+    }
+
+    public static void addUserPassList(String userPass) {
+        User.userPassList.add(userPass)
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public int getPlaycount() {
+        return Playcount;
+    }
+
+    public void setPlaycount(int playcount) {
+        Playcount = playcount;
+    }
+
+    public ArrayList getDeck() {
+        return deck;
+    }
+
+    public void setDeck(ArrayList deck) {
+        this.deck = deck;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userID=" + userID + ", userName=" + userName + ", userPass=" + userPass + ", Playcount="
+                + Playcount + ", deck=" + deck + "]";
     }
 
     
