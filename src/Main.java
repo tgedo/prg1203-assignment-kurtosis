8540b8d3712cca2872d0a795b480faba44b746ba
Alex Chee <xxchee555xx@gmail.com>
2023-12-07 00:39:53 +0800

    stuff

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     int playerDecision;
 
     public static void main(String[] args) {
+        //create a new dummy user with manual id here cause code shit
+        User dummy = new User(69000, "Your Mum", "Ash Ketchump")
 
     }
 
@@ -13,6 +15,7 @@ public class Main {
         String[] options = {"Continue","New","Leaderboard","Quit"};
         String errorMessage = "";
 
+        //TODO can actually make all these into a method
         while (true) {
             for(int choice = 1; choice < options.length; choice++){
             System.out.println(choice + " : " + options[choice-1]);
@@ -28,13 +31,42 @@ public class Main {
         }
 
         //Player chooses Continue
+        while (true) {
+            System.out.println("Enter a Username");
+            String newUserName = sc.nextLine();
+            System.out.println("Enter a Password");
+            String newUserPass = sc.nextLine();
+            
+        }
+        //TODO add login here
+
         if (playerDecision == 1){
-            for(int userIndex = 1; userIndex < User.userNameList; userIndex++){
-                
+            while (true) {
+                for(int userIndex = 1; userIndex < User.userNameList.size(); userIndex++){
+                System.out.println(userIndex + " : " + User.userNameList.get(userIndex-1));
+                System.out.println("\n Choose an option!");
+                System.out.println(errorMessage);
+            }
+                playerDecision = sc.nextInt();
+                if (playerDecision > User.userNameList.size() || playerDecision < 1){
+                    errorMessage = "Pick a number between 1 and " + User.userNameList.size();
+                    continue;
+                }
+                    break;
             }
         }
 
         //Player chooses New
+        else if (playerDecision == 2){
+            System.out.println("Creating a new account...");
+            System.out.println("Enter a Username");
+            String newUserName = sc.nextLine();
+            System.out.println("Enter a Password");
+            String newUserPass = sc.nextLine();
+            
+            //generate a new user here idk how help pls
+            
+        }
 
         //Player chooses Leaderboard
 
--- src/User.java
+++ src/User.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
 
 public class User {
-    static ArrayList userIDList = new ArrayList<String>();
+    static ArrayList userIDList = new ArrayList<Integer>();
     static ArrayList userNameList = new ArrayList<String>();
     static ArrayList userPassList = new ArrayList<String>();
 
-    private String userID;
+    private int userID;
     private String userName;
     private String userPass;
-
-    private int Playcount;
+    private int Playcount = 0;
     ArrayList deck = new ArrayList<Pokemon>();
+
+    public User(int userID, String userName, String userPass) {
+        this.userID = userID;
+        this.userName = userName;
+        this.userPass = userPass;
+    }
+    
+    public User(String userName, String userPass) {
+        this.userID = (userIDList.get(userIDList.size()-1)) + 1; //help
+        this.userName = userName;
+        this.userPass = userPass;
+    }
 
     public void GeneratePokemon(String name, int HP, int ATK, int DEF, int SPD, int SPE, char type, int movePower, Boolean moveIsSpecial){
         // TODO figure this part out thanks
